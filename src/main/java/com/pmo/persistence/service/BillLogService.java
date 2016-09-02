d90eb35a8a2f67b302c9b8c49559686057240c89
Vijay Ganesan <Vijay.Ganesan@klm.com>
2016-09-02 13:58:19 +0200

    Jtable: Implemented pagination

--- src/main/java/com/pmo/persistence/service/BillLogService.java
+++ src/main/java/com/pmo/persistence/service/BillLogService.java
@@ -19,6 +19,11 @@ public class BillLogService {
 
     public static final String COLLECTION_NAME = "billLog";
 
+    /**
+     * Save the record in database
+     * @param billLogBean
+     * @return
+     */
     public BillLogBean save(BillLogBean billLogBean) {
         if (!mongoTemplate.collectionExists(COLLECTION_NAME)) {
             mongoTemplate.createCollection(COLLECTION_NAME);
@@ -37,22 +42,34 @@ public class BillLogService {
         return settingsBean;
     }
 
-    
-    public List<BillLogBean> listGet() {
-        List<BillLogBean> settingss = mongoTemplate.findAll(BillLogBean.class);
-        mongoTemplate.findAll(BillLogBean.class);
-        return settingss;
-    }
-    
+    /**
+     * Get the list of items with Indexing for Pagination. 
+     * @param jtStartIndex
+     * @param jtPageSize
+     * @param jtSorting
+     * @return
+     */
     public List<BillLogBean> list(int jtStartIndex, int jtPageSize, String jtSorting) {
-    	Query query = new Query(Criteria.where("_id").is(1));
-//        List<BillLogBean> settingss = mongoTemplate.findAll(BillLogBean.class);
+    	Query query = new Query();
+    	query.skip(jtStartIndex);
+    	query.limit(jtPageSize);
     	List<BillLogBean> settingss = mongoTemplate.find(query, BillLogBean.class);
         return settingss;
     }
 
+    /**
+     * Deleting the particular record in the table.
+     * @param id
+     */
     public void delete(String id) {
         Query query = new Query(Criteria.where("_id").is(id));
         mongoTemplate.remove(query, COLLECTION_NAME);
+    }
+    
+    // Just for testing purpose
+    public List<BillLogBean> listGet() {
+        List<BillLogBean> settingss = mongoTemplate.findAll(BillLogBean.class);
+        mongoTemplate.findAll(BillLogBean.class);
+        return settingss;
     }
 }
